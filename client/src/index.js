3088bf8551c53358e0d9afb487981249cb9b2dc8
Sanderhelleso <sanderhelleso@hotmail.com>
2018-09-19 01:55:46 -0700

    final commit for code, added gif for readme and media queries for responsive styles

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
 import Stopwatch from './components/Stopwatch';
 
 ReactDOM.render(
     <Stopwatch />,
      document.getElementById('root')
 );
-
-registerServiceWorker();
